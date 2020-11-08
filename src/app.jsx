897b9703687cab776e0d8515394bb284e4f33cbe
apple <l@MacBook-Pro.local>
2020-11-08 18:11:03 +0800

    .

--- src/app.jsx
+++ src/app.jsx
@@ -1,49 +1,48 @@
-import React from "react";
-import { hot } from "react-hot-loader/root";
+import React from 'react';
+import { hot } from 'react-hot-loader/root';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { date: new Date(), text: 0 };
-    this.numbers = [1, 2, 4, 5, 6, 7, 8];
-  }
+    constructor(props) {
+        super(props);
+        this.state = { date: new Date(), text: 0 };
+        this.numbers = [1, 2, 4, 5, 6, 7, 8];
+    }
 
-  componentDidMount() {
-    this.timerID = setInterval(() => this.tick(), 1000);
-  }
+    componentDidMount() {
+        this.timerID = setInterval(() => this.tick(), 1000);
+    }
 
-  componentWillUnmount() {
-    clearInterval(this.timerID);
-  }
+    componentWillUnmount() {
+        clearInterval(this.timerID);
+    }
 
-  tick() {
-    this.setState({
-      date: new Date(),
-    });
-  }
+    tick() {
+        this.setState({
+            date: new Date(),
+        });
+    }
 
-  handleClick() {
-    console.log(1);
-    this.setState({
-      text: ++this.state.text,
-    });
-  }
+    handleClick() {
+        this.setState({
+            text: this.state.text + 1,
+        });
+    }
 
-  render() {
-    return (
-      <div>
-        <h1 onClick={() => this.handleClick()}>
-          Hello, world!{this.state.text}
-        </h1>
-        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
-        <ul>
-          {this.numbers.map((number) => (
-            <li key={number.toString()}>{number}</li>
-          ))}
-        </ul>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                <h1 onClick={() => this.handleClick()}>
+                    Hello, world!{this.state.text}
+                </h1>
+                <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+                <ul>
+                    {this.numbers.map((number) => (
+                        <li key={number.toString()}>{number}</li>
+                    ))}
+                </ul>
+            </div>
+        );
+    }
 }
 
 export default hot(App);
--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app.jsx';
+// import ToDoList from './todo-list.jsx';
 
-ReactDOM.render(
- <App/>,
- document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
