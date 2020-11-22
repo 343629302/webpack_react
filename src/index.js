c61efee3ff6caa4694662fac160cc4d1d58b9c00
apple <l@MacBook-Pro.local>
2020-11-22 14:29:02 +0800

    。

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import App from './app.jsx';
+import App from './app.jsx';
 import ToDoList from './todo-list.jsx';
 import 'antd/dist/antd.css';
 import './scss/app.scss';
 import { Provider } from 'react-redux';
 import store from './store/index.js';
+import { HashRouter, Route } from 'react-router-dom';
 
 ReactDOM.render(
     <Provider store={store}>
-        <ToDoList />
+        {/* hash路由  */}
+        <HashRouter>
+            {/* path:地址  exact:是否必须全等于才显示，最好有这个参数   render:渲染的页面    */}
+            <Route path="/" exact render={() => <ToDoList />}></Route>
+            <Route path="/detail" exact render={() => <App />}></Route>
+        </HashRouter>
     </Provider>,
     document.getElementById('root')
 );
